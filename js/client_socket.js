a6f5d7a05afc226d9a5fb00ecc74c82ca375b5cb
d.larchikov <d.larchikov@b2b-center.ru>
2014-12-18 21:06:28 +0300

    fix

--- js/client_socket.js
+++ js/client_socket.js
@@ -2,8 +2,8 @@
  * Created by dlarchikov on 18.12.14.
  */
 
-window.onload = function () {
-    var socket = io.connect('http://localhost:8080');
+//window.onload = function () {
+    var socket = io.connect('http://' + window.location.host + ':3110');
     var cEvents = new CEvents(socket);
 
     socket.on('connect', function () {
@@ -11,7 +11,7 @@ window.onload = function () {
             'event': 'auth',
             'role': 'server'
         });
-
+        //
 
         socket.on('message', function (msg) {
             switch (msg.event) {
@@ -20,4 +20,4 @@ window.onload = function () {
             }
         });
     });
-};
+//};
